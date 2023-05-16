3be67d82ca21f2e0da7146c8f52e8daee7217baa
engmondy <108227192+engmondy@users.noreply.github.com>
2023-05-16 21:23:28 +0300

    Update OpenAI.ts

--- packages/components/nodes/llms/OpenAI/OpenAI.ts
+++ packages/components/nodes/llms/OpenAI/OpenAI.ts
@@ -32,6 +32,12 @@ class OpenAI_LLMs implements INode {
                 type: 'options',
                 options: [
                     {
+                        label: 'gpt-3.5-turbo',
+                        name: 'gpt-3.5-turbo'
+                    },
+                    {
+                    
+                    {
                         label: 'text-davinci-003',
                         name: 'text-davinci-003'
                     },
